14aa8a4122cc223cf6be1c03ed2932b651c4c413
Akira Saito <akira0517@icloud.com>
2015-04-23 18:56:47 +0900

    Initial Commit

---
+++ count1234567/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  count1234567
+//
+//  Created by Akira Saito on 2015/04/23.
+//  Copyright (c) 2015年 AkiraSaito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ count1234567/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  count1234567
+//
+//  Created by Akira Saito on 2015/04/23.
+//  Copyright (c) 2015年 AkiraSaito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

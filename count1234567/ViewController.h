077bcc2daca8d1a026072fb08949d11b83bae8fd
Akira Saito <akira0517@icloud.com>
2015-04-23 19:27:57 +0900

    初commit&push

--- count1234567/ViewController.h
+++ count1234567/ViewController.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ViewController : UIViewController{
+    int number;
+    IBOutlet UILabel *label;
+}
+-(IBAction)plus;
+-(IBAction)minus;
 
 @end
 
--- count1234567/ViewController.m
+++ count1234567/ViewController.m
@@ -18,7 +18,15 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+-(IBAction)plus{
+    number=number+1;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
 
+-(IBAction)minus{
+    number=number+1;
+    label.text=[NSString stringWithFormat:@"%d",number];
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
